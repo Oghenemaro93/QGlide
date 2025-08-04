4712ae1c33a88e2473d4c9cbb7b34ae4f2fee942
Roosevelt <rooseveltabandy@gmail.com>
2025-08-04 14:55:42 +0100

    update login

--- config/urls.py
+++ config/urls.py
@@ -44,4 +44,5 @@ urlpatterns = [
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
+    path("v1/auth/", include("core.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
