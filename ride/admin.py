473487e9287916805ed24a5e4f18a31d7af32f2e
Roosevelt <rooseveltabandy@gmail.com>
2025-09-21 16:25:23 +0100

    update

--- ride/admin.py
+++ ride/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
+from . import resources
+from .models import (
+    Ride
+)
 # Register your models here.
+
+class RideResourceAdmin(ImportExportModelAdmin):
+    resource_class = resources.RideResource
+
+    # search_fields = ["ride_type"]
+    # list_filter = ["is_active"]
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+    
+
+
+admin.site.register(Ride, RideResourceAdmin)
