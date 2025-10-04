594c1a773e6cbe175bee4414459f26dcf3f5471e
Chikezie Ndubuisi <chikeziendubuisi7@gmail.com>
2025-10-04 07:45:12 +0100

    Temporary OTP fix for testing registration bug fix

--- core/helpers/func.py
+++ core/helpers/func.py
@@ -3,4 +3,6 @@ import string
 
 
 def generate_verification_code():
-    return "".join(random.choices(string.digits + string.digits, k=4))
+    # For testing purposes, use a fixed OTP code
+    return "1234"
+    # return "".join(random.choices(string.digits + string.digits, k=4))
